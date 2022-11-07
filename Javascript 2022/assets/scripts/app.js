2da42115a6e9f9c394d28bbea6183005dfd13ab7
ninnistic <bewithu14@gmail.com>
2022-11-08 00:00:10 +0900

    221107 ► JS : Functions

--- Javascript 2022/assets/scripts/app.js
+++ Javascript 2022/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserNumberInput() {
    return parseInt(userInput.value)
@@ -13,8 +14,12 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
 function add() {
    const enteredNumber = getUserNumberInput ();
    const initialResult = currentResult;  
-   currentResult = currentResult + enteredNumber;
+   currentResult += enteredNumber;
    createAndWriteOutput('+', initialResult, enteredNumber)
+   logEntries.push(enteredNumber);
+   //.push 배열에 변수이름을 넣어, 값을 넣을 수 있다.
+   // like, logEntries = [enteredNumber]; 와 같다. 
+   console.log(logEntries[1]); 
 }
 /*내부 상수와 외부 전역의 값을 섞어서 사용하지 않고,
 오로지 전역값만을 사용해 이벤트 리스너로 사용되는 점에서는 괜찮다. */ 
@@ -23,21 +28,21 @@ function add() {
 function subtract() {
    const enteredNumber = getUserNumberInput();
    const initialResult = currentResult;  
-   currentResult = currentResult - enteredNumber;
+   currentResult -= enteredNumber;
    createAndWriteOutput('-', initialResult, enteredNumber)
 }
 
 function multiply() {
    const enteredNumber = getUserNumberInput();
    const initialResult = currentResult;  
-   currentResult = currentResult * enteredNumber;
+   currentResult *= enteredNumber;
    createAndWriteOutput('*', initialResult, enteredNumber)
 }
 
 function divide() {
    const enteredNumber = getUserNumberInput();
    const initialResult = currentResult;  
-   currentResult = currentResult / enteredNumber;
+   currentResult /= enteredNumber;
    createAndWriteOutput('/', initialResult, enteredNumber)
 }
 
@@ -49,4 +54,4 @@ addBtn.addEventListener('click', add);
 /* 즉, 브라우저에게 add라는 함수의 주소를 알려주고, 버튼이 클릭되면 함수를 호출하도록 한다.*/
 subtractBtn.addEventListener('click', subtract);
 multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click',divide);
+divideBtn.addEventListener('click', divide);
