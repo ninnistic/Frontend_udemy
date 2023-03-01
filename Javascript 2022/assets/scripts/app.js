d20b612cdf1c66070a7429b292e2e4bb95f7e0ef
ninnistic <bewithu14@gmail.com>
2023-03-01 15:09:10 +0900

    20230301

--- Javascript 2022/assets/scripts/app.js
+++ Javascript 2022/assets/scripts/app.js
@@ -7,43 +7,63 @@ function getUserNumberInput() {
 }
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-   const calcDescription =  `${resultBeforeCalc} ${operator} ${calcNumber}`
+   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
    outputResult(currentResult, calcDescription)
+}// from vendor file
+
+function writeToLog(
+   operationIdentifier,
+   prevResult,
+   operationNumber,
+   newResult
+) {
+   const logEntry = {
+      operation: operationIdentifier,
+      prevResult: prevResult,
+      number: operationNumber,
+      result: newResult
+   };
+   logEntries.push(logEntry);
+   console.log(logEntry.operation);
+   console.log(logEntries);
 }
 
+
 function add() {
-   const enteredNumber = getUserNumberInput ();
-   const initialResult = currentResult;  
+   const enteredNumber = getUserNumberInput();
+   const initialResult = currentResult;
    currentResult += enteredNumber;
-   createAndWriteOutput('+', initialResult, enteredNumber)
-   logEntries.push(enteredNumber);
-   //.push 배열에 변수이름을 넣어, 값을 넣을 수 있다.
-   // like, logEntries = [enteredNumber]; 와 같다. 
-   console.log(logEntries[1]); 
+   createAndWriteOutput('+', initialResult, enteredNumber);
+   writeToLog('ADD', initialResult, enteredNumber, currentResult);
 }
+
+
 /*내부 상수와 외부 전역의 값을 섞어서 사용하지 않고,
-오로지 전역값만을 사용해 이벤트 리스너로 사용되는 점에서는 괜찮다. */ 
+오로지 전역값만을 사용해 이벤트 리스너로 사용되는 점에서는 괜찮다. */
 /* 이런 함수는...return 해서는 안된다.. : 할수는 있는데, 하면 전역변수의 값을 바로 수정해 버릴것  */
 
 function subtract() {
    const enteredNumber = getUserNumberInput();
-   const initialResult = currentResult;  
+   const initialResult = currentResult;
    currentResult -= enteredNumber;
    createAndWriteOutput('-', initialResult, enteredNumber)
+   writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
 }
 
 function multiply() {
    const enteredNumber = getUserNumberInput();
-   const initialResult = currentResult;  
+   const initialResult = currentResult;
    currentResult *= enteredNumber;
    createAndWriteOutput('*', initialResult, enteredNumber)
+   writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
 }
 
 function divide() {
    const enteredNumber = getUserNumberInput();
-   const initialResult = currentResult;  
+   const initialResult = currentResult;
    currentResult /= enteredNumber;
    createAndWriteOutput('/', initialResult, enteredNumber)
+   writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
 }
 
 
